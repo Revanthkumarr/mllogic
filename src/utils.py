6a0570998166775cc292efa008c891de97c97240
Revanthkumarr <revanthkumarr15@gmail.com>
2023-07-17 22:57:25 -0500

    Website build

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,8 @@ import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 
+from sklearn.model_selection import GridSearchCV
+
 
 def save_object(file_path,obj):
     try:
@@ -22,14 +24,19 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train,Y_train,X_test,Y_test,models):
+def evaluate_model(X_train,Y_train,X_test,Y_test,models,parms):
 
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=parms[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,Y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,Y_train)
 
             y_train_pred=model.predict(X_train)
@@ -46,6 +53,13 @@ def evaluate_model(X_train,Y_train,X_test,Y_test,models):
     except Exception as e:
         raise CustomException(e,sys)    
 
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)    
 
 
 
